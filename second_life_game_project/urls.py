1b681b8fcc160a9f3112a1580909a047634543db
Jinyi Li <2521896l@student.gla.ac.uk>
2022-02-08 19:18:48 +0000

    modify webhook url

--- second_life_game_project/urls.py
+++ second_life_game_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('github_webhook/', include('git_webhook.urls')),
+    path('git_push/', include('git_webhook.urls')),
 ]
