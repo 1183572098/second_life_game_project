1b649e5d1da13d0cf8dcdfc4e04a741a586857a0
Jinyi Li <2521896l@student.gla.ac.uk>
2022-02-08 20:22:46 +0000

    Add reload web after webhook

--- git_webhook/views.py
+++ git_webhook/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import git
+import os
 
 
 @csrf_exempt
 def github_webhook(request):
     repo = git.Repo('./second_life_game_project')
     origin = repo.remotes.origin
-    print(origin.pull())
+    origin.pull()
+    os.system('/var/www/caxiao_pythonanywhere_com_wsgi.py')
+    print("reload web success!")
     return HttpResponse(status=200)
