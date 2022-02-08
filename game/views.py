be7677f519bc49800f6cafc6fe4eebfe51fe41ec
Jinyi Li <2521896l@student.gla.ac.uk>
2022-02-08 20:44:09 +0000

    Test webhook

--- game/views.py
+++ game/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     print("This is index page")
-    return HttpResponse("This is index Page. Testing...")
+    return HttpResponse("This is index Page. Testing")
