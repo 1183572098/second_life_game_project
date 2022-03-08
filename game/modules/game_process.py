dbf7999721d8b298cf2507657f9a5f5229554bd4
Jinyi Li <2521896l@student.gla.ac.uk>
2022-03-08 19:55:02 +0000

    code specification

--- game/modules/game_process.py
+++ game/modules/game_process.py
@@ -23,7 +23,7 @@ class Process:
         random_numbers = [random.random() for _ in range(len(self.role.attribute))]
         random_sum = sum(random_numbers)
         random_attributes = [int(value/random_sum*parameter.value(2001)) for value in random_numbers]
-        if sum(random_attributes) != 100:
+        if sum(random_attributes) != int(parameter.value(2001)):
             random_attributes[-1] += 1
         index = 0
         for key in self.role.attribute.keys():
