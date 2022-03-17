fb86e225d8c5d3f6e4c01830f20c2f53bfc30dde
Jinyi Li <2521896l@student.gla.ac.uk>
2022-03-17 13:18:09 +0000

    fix bug

--- static/js/store.js
+++ static/js/store.js
@@ -1,4 +1,4 @@
-var store = byId("store"),
+var wholeStore = byId("wholeStore"),
 	subMiddle = byId("subMiddle"),
 	subMiddle2 = byId('subMiddle2'),
 	flag=false;
@@ -35,12 +35,12 @@ function readTableSuccess(data){
 
 function openBag(){
 	if(flag){
-		store.className="store hide";
+		wholeStore.className="store hide";
 		subMiddle.className="subMiddle active";
 		subMiddle2.className="subMiddle2 active"
 		flag=false;
 	}else{
-		store.className="store active";
+		wholeStore.className="store active";
 		subMiddle.className="subMiddle hide";
 		subMiddle2.className="subMiddle2 hide";
 		flag=true;
