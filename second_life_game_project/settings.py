a7c77187e0a182b853e93661a37dc4b246fec704
Jinyi Li <98088982+1183572098@users.noreply.github.com>
2022-03-18 14:02:51 +0000

    Update settings.py

--- second_life_game_project/settings.py
+++ second_life_game_project/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'second_life_game_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'second_life',  #second_life
-        'USER': 'root',  #root 
-        'PASSWORD': '',  #123456
-        'HOST': 'localhost',  # localhost
+        'NAME': 'CAXiao$second_life',  #second_life
+        'USER': 'CAXiao',  #root 
+        'PASSWORD': 'secondlife',  #123456
+        'HOST': 'CAXiao.mysql.pythonanywhere-services.com',  # localhost
         'PORT': '3306',
     }
 }
